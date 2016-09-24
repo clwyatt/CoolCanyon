eff5f25498c77b91dfb087acb1a6e448aad08f9c
Chris Wyatt <clwyatt@vt.edu>
2016-09-24 16:32:55 -0400

    switch to first person, add some more images

--- main.py
+++ main.py
@@ -15,17 +15,6 @@ DOWN = 274
 LEFT = 275
 RIGHT = 276
 
-rocket_vertices = ((-1,-1),(0,1),(1,-1),(-1,-1),(-0.5,-1.25),(0,-1),(0.5,-1.25),(1,-1))
-
-def transform(vertices, center, orient, scale):
-	screen = []
-	for v in vertices:
-		r = scale*(cos(orient+pi)*v[0] - sin(orient+pi)*v[1]) + center[0]
-		c = scale*(sin(orient+pi)*v[0] + cos(orient+pi)*v[1]) + center[1]
-		screen.append((r,c))
-
-	return screen
-
 def draw_rocket(surf, center, orient, scale):
 	pygame.draw.polygon(surf, BLACK, transform(rocket_vertices, center, orient, scale), 1)
 
@@ -37,8 +26,15 @@ if __name__ == "__main__":
 
 	pygame.display.set_caption("Cool Canyon")
 
-	background = pygame.image.load('canyon1.png')
+	background = pygame.image.load('images/window.png')
 	background_rect = background.get_rect()
+
+	mothership = pygame.image.load('images/mothership.png')
+	mothership_rect = mothership.get_rect()
+
+	saucer = pygame.image.load('images/saucer.png')
+	saucer_rect = saucer.get_rect()
+
 	
  	# Loop until the user clicks the close button.
 	done = False
@@ -46,45 +42,35 @@ if __name__ == "__main__":
 	# Used to manage how fast the screen updates
 	clock = pygame.time.Clock()
 
-	center = [100,200]
-	scale = 20
-	forward_speed = 0
-	orient = pi
+	dx = 0
+	dy = 0
 	
 	while not done:
 
 		screen.blit(background,background_rect)
+		screen.blit(saucer,saucer_rect.move(100+dx,100+dy))
+		#screen.blit(mothership,mothership_rect.move(300,50))
 		
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				done = True
-			if event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_DOWN:
-					forward_speed += 0.1
-					if(forward_speed > 1): forward_speed = 1
-				if event.key == pygame.K_UP:
-					forward_speed -= 0.1
-					if(forward_speed < -1): forward_speed = -1
+			#if event.type == pygame.KEYDOWN:
+				#if event.key == pygame.K_DOWN:
+
+				#if event.key == pygame.K_UP:
 					
 		keys = pygame.key.get_pressed() 
 		if keys[pygame.K_LEFT]:
-			orient += -0.02
+			dx += 5
 		if keys[pygame.K_RIGHT]:
-			orient += 0.02
+			dx += -5
+		if keys[pygame.K_UP]:
+			dy += 5
+		if keys[pygame.K_DOWN]:
+			dy += -5
 					
 		# --- Game logic should go here
-
-		center[0] += forward_speed*cos(orient+pi/2)
-		center[1] += forward_speed*sin(orient+pi/2)
-		
-		if background.get_at((int(center[0]), int(center[1]))) != (46,127,227, 255): 
-			forward_speed = 0
-		   		
-		###screen.fill(WHITE)
-				
-		# --- Drawing code should go here
-		draw_rocket(screen, center, orient, scale)
-		
+		   				
 		pygame.display.flip()
 		
 		clock.tick(60)
